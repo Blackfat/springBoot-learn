6613ac7c0429e5eaa6d95e03e11c7f0ef44ff231
wangfeiyang <wangfeiyang@huaxiafinance.com>
2017-04-07 15:06:17 +0800

    添加全局异常的处理

---
+++ Chapter1/src/main/java/com/blackfat/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.blackfat;
+
+import com.blackfat.domain.ErrorInfo;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author wangfeiyang
+ * @desc
+ * @create 2017/4/7-14:52
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, Exception e){
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setMessage(e.getMessage());
+        r.setCode(ErrorInfo.ERROR);
+        r.setData("系统异常，请稍后再试！");
+        r.setUrl(req.getRequestURL().toString());
+        return r;
+    }
+}
---
+++ Chapter1/src/main/java/com/blackfat/web/GlobalExceptionController.java
@@ -0,0 +1,20 @@
+
+package com.blackfat.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author wangfeiyang
+ * @desc
+ * @create 2017/4/7-14:59
+ */
+@Controller
+public class GlobalExceptionController {
+
+    @RequestMapping(value="/exception",method = RequestMethod.GET)
+    public String exceptionHandler() throws Exception{
+        throw new Exception("发生异常！");
+    }
+}
