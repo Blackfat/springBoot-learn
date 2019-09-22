db29c9ee61962cc6e24236aa1d3a6cbc78b76f77
wangfeiyang <wangfeiyang08498@hellobike.com>
2019-09-22 20:15:49 +0800

    boot-drools

---
+++ boot-drools/src/main/java/com/blackfat/bootdrools/entity/User.java
@@ -0,0 +1,21 @@
+package com.blackfat.bootdrools.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author wangfeiyang
+ * @Description
+ * @create 2019-09-22 19:23
+ * @since 1.0-SNAPSHOT
+ */
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String name;
+
+    private int age;
+
+
+}
